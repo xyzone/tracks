828e617f56564a61c5d37703acdb9c4b2170808c
Richard Liu <bluetoliu@gmail.com>
2019-09-09 23:49:02 +1000

    map

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Text} from 'react-native-elements';
 import {SafeAreaView} from 'react-navigation'
-import MapView from 'react-native-maps';
+import Map from '../components/Map';
+import {requestPermissionsAsync} from 'expo-location';
 
 const TrackCreateScreen = () => {
+    const [err, setErr] = useState(null)
+    const startWatching = async() => {
+        try{
+            await requestPermissionsAsync()
+        }catch(e){
+            setErr(e)
+        }
+    }
+
+    useEffect(
+       () => {startWatching()}
+    , [])
     return( 
         <SafeAreaView force-inset={{top: 'always'}}> 
         
             <Text h3>
                 Track Create Screen  </Text> 
-            <MapView style={styles.map} />
+            <Map  />
+            {err ? <Text>Please Enable location service</Text>: null}
         
         </SafeAreaView>
     )
 } 
 
 const styles = StyleSheet.create({
-    map: {
-        height: 300, 
-    }
+  
 })
 
 export default TrackCreateScreen
